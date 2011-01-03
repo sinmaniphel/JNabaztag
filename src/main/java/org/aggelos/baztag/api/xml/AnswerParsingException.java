341b885cd6bae36088114108b7a14458ac909461
aggelos <aggelos@julietta>
2011-01-03 23:54:18 +0100

    lacking in source

---
+++ src/main/java/org/aggelos/baztag/api/xml/AnswerParsingException.java
@@ -0,0 +1,24 @@
+package org.aggelos.baztag.api.xml;
+
+public class AnswerParsingException extends Exception {
+
+	public AnswerParsingException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public AnswerParsingException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AnswerParsingException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AnswerParsingException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
