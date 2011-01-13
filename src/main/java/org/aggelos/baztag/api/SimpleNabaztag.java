0d88c71f027845b549659a7c906a113e03eacf26
Thibaud Vibes <tvibes@onyme.com>
2011-01-13 14:21:14 +0100

    - add sleep(), awake() methods - code duplication: change #say(String) to call #say(String, String)

--- src/main/java/org/aggelos/baztag/api/SimpleNabaztag.java
+++ src/main/java/org/aggelos/baztag/api/SimpleNabaztag.java
@@ -4,6 +4,7 @@ import org.aggelos.baztag.api.inst.LeftEarInstruction;
 import org.aggelos.baztag.api.inst.RightEarInstruction;
 import org.aggelos.baztag.api.inst.TextInstruction;
 import org.aggelos.baztag.api.inst.VoiceInstruction;
+import org.aggelos.baztag.api.inst.WakeUpInstruction;
 import org.aggelos.baztag.api.parts.Lang;
 
 
@@ -30,10 +31,7 @@ public class SimpleNabaztag extends Nabaztag {
 	 * @return true if the message was transmitted correctly, false otherwise
 	 */
 	public boolean say(String message) {
-		NabaztagInstructionSequence seq = new NabaztagInstructionSequence();
-		TextInstruction ti = new TextInstruction(message);
-		seq.add(ti);
-		return execute(seq);
+		return say(message, null);
 	}
 	
 	/**
@@ -47,9 +45,10 @@ public class SimpleNabaztag extends Nabaztag {
 	public boolean say(String message, String voice) {
 		NabaztagInstructionSequence seq = new NabaztagInstructionSequence();
 		TextInstruction ti = new TextInstruction(message);
-		VoiceInstruction vi = new VoiceInstruction(voice);
 		seq.add(ti);
-		seq.add(vi);
+		if(voice !=null)
+			seq.add(new VoiceInstruction(voice));
+		
 		return execute(seq);
 	}
 	
@@ -75,6 +74,28 @@ public class SimpleNabaztag extends Nabaztag {
 		RightEarInstruction lei = new RightEarInstruction((short)position);
 		seq.add(lei);
 		return execute(seq);
+	}
+	
+	/**
+	 * Will shut down the nabaztag
+	 * @return
+	 * 		TRUE if the instruction was successfully sent
+	 */
+	public boolean sleep() {
+		NabaztagInstructionSequence seq = new NabaztagInstructionSequence();
+		seq.add(new WakeUpInstruction(false));	
+		return this.execute(seq);		
+	}
+	
+	/**
+	 * Will awake the nabaztag
+	 * @return
+	 * 		TRUE if the instruction was successfully sent
+	 */
+	public boolean awake() {
+		NabaztagInstructionSequence seq = new NabaztagInstructionSequence();
+		seq.add(new WakeUpInstruction(true));	
+		return this.execute(seq);
 	}
 
 }
