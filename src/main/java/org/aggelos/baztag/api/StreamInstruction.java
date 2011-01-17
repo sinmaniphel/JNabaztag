9d1a791a52b367eeb9c20d6e056df94acbd43c70
Thibaud Vibes <tvibes@onyme.com>
2011-01-17 19:46:36 +0100

    Add "stream instruction interface"

---
+++ src/main/java/org/aggelos/baztag/api/StreamInstruction.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.aggelos.baztag.api;
+
+/**
+ * Interface that design a subset of {@link NabaztagInstruction}
+ * 
+ * @see NabaztagInstruction
+ * @author tvibes
+ *
+ */
+public interface StreamInstruction{
+
+	public String getStreamLocation();
+}
